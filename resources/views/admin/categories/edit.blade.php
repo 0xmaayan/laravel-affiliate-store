fdc3ea8c273cd6b6b110eabaaabd64e32553ace7
msbir <maayan@b2cprint.com>
2017-11-12 21:34:13 +0200

    products view

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -57,45 +57,4 @@
         </script>
     </div>
 </div>
-
-<script>
-    var template = Handlebars.compile($("#details-template").html());
-
-    var table = $('#products-table').DataTable({
-        processing: true,
-        serverSide: true,
-        ajax: '{{route('categories.index')}}',
-        columns: [
-            {
-                "className":      'details-control',
-                "orderable":      false,
-                "searchable":     false,
-                "data":           null,
-                "defaultContent": ''
-            },
-            {data: 'id', name: 'id'},
-            {data: 'name', name: 'name'},
-            {data: 'created_at', name: 'created_at'},
-            {data: 'updated_at', name: 'updated_at'},
-        ],
-        order: [[1, 'asc']]
-    });
-
-    // Add event listener for opening and closing details
-    $('#products-table tbody').on('click', 'td.details-control', function () {
-        var tr = $(this).closest('tr');
-        var row = table.row( tr );
-
-        if ( row.child.isShown() ) {
-            // This row is already open - close it
-            row.child.hide();
-            tr.removeClass('shown');
-        }
-        else {
-            // Open this row
-            row.child( template(row.data()) ).show();
-            tr.addClass('shown');
-        }
-    });
-</script>
 @endsection
