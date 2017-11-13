829b37fc824f74b9cdfc9ee0b87c068b2f87b7a6
msbir <maayan@b2cprint.com>
2017-11-13 11:44:58 +0200

    style edit cat section

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -16,18 +16,20 @@
             </h1>
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-12">
             {!! Form::open(['route' => ['categories.update',$category->id ],'files' => true ,'method' => 'PUT']) !!}
-            <div class="form-group">
+            <div class="form-group col-md-4">
                 <label>{!! Form::label('name') !!}</label>
                 {!! Form::text('name',$category->name,['class' => 'form-control']) !!}
             </div>
-            <div class="form-group">
+            <div class="form-group col-md-4">
                 <label>{!! Form::label('image') !!}</label>
                 {!! Form::file('image') !!}
+            </div>
+            <div class="form-group col-md-4">
                 <img width="150" height="150" src="{{asset('images/categories/'.$category->image)}}" alt="category image">
             </div>
-            {!! Form::submit('Update',['class' => 'btn btn-default']) !!}
+            {!! Form::submit('Update',['class' => 'btn btn-default col-md-12']) !!}
             {!! Form::close() !!}
         </div>
 
@@ -39,22 +41,7 @@
         </div>
 
         <div id="products-table"></div>
-        <script id="details-template" type="text/x-handlebars-template">
-            <table class="table">
-                <tr>
-                    <td>Full name:</td>
-                    <td>{{name}}</td>
-                </tr>
-                <tr>
-                    <td>Email:</td>
-                    <td>{{email}}</td>
-                </tr>
-                <tr>
-                    <td>Extra info:</td>
-                    <td>And any further details here (images etc)...</td>
-                </tr>
-            </table>
-        </script>
+
     </div>
 </div>
 @endsection
--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,7 +1,19 @@
-
-
-
-
+<script id="details-template" type="text/x-handlebars-template">
+<table class="table">
+<tr>
+<td>Full name:</td>
+<td>name</td>
+</tr>
+<tr>
+<td>Email:</td>
+<td>email</td>
+</tr>
+<tr>
+<td>Extra info:</td>
+<td>And any further details here (images etc)...</td>
+</tr>
+</table>
+</script>
 
 
 
