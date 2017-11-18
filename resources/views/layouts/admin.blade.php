acb9c1433e4dcadef3ab9d5a2b2c1b55ac01654f
msbir <maayan@b2cprint.com>
2017-11-18 16:46:38 +0200

    display current user name

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -59,7 +59,7 @@
                                     <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                                 </a>
                                 <ul class="dropdown-menu dropdown-user">
-                                    <li><a href="#"><i class="fa fa-user fa-fw"></i> My Profile</a>
+                                    <li><a href="#"><i class="fa fa-user fa-fw"></i> {{Auth::user()->name}}</a>
                                     </li>
                                     <li class="divider"></li>
                                     <li>
