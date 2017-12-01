5290e675a49f89f8e66722f59203575c787681cf
msbir <maayan@b2cprint.com>
2017-12-01 17:34:49 +0200

    get products relation to categories by eloquent

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -90,7 +90,7 @@ class CategoriesController extends AdminController
     public function edit($id, Request $request)
     {
         $category = Category::findOrFail($id);
-        $products = Product::where('category_id',$id)->get();
+        $products = $category->products()->get();
 
       if ($request->ajax()){
         return Datatables::of($products)
