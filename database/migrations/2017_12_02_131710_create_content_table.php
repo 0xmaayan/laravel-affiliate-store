c116754dc18a0bcd7930f019160ac2061e0748e5
msbir <maayan@b2cprint.com>
2017-12-02 15:24:25 +0200

    create content table

---
+++ database/migrations/2017_12_02_131710_create_content_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->json('files')->nullable();
+            $table->json('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+}
