6966197509e2dc593860e8d15c1c9f092b8c6535
msbir <maayan@b2cprint.com>
2017-12-17 20:04:05 +0200

    make uploads folder

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -31,7 +31,7 @@
                     {!! Form::file('image') !!}
                 </div>
                 <div class="form-group col-md-4">
-                    <img width="150" height="150" src="{{asset('images/categories/'.$category->image)}}" alt="category image">
+                    <img width="150" height="150" src="{{asset('uploads/categories/'.$category->image)}}" alt="category image">
                 </div>
                 {!! Form::submit('Update',['class' => 'btn btn-default col-md-12']) !!}
                 {!! Form::close() !!}
--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -21,7 +21,7 @@
             <div class="col-md-4" style="">
                 <div class="card text-center">
                     <a href="{{route('admin.categories.edit',['id' => $category->id ])}}">
-                        <img class="card-img-top" style="width: 200px;height:200px;object-fit: contain;" src="{{asset('images/categories/'.$category->image)}}" alt="category image">
+                        <img class="card-img-top" style="width: 200px;height:200px;object-fit: contain;" src="{{asset('uploads/categories/'.$category->image)}}" alt="category image">
                         <div class="card-block">
                             <h4 class="card-title">{{$category->name}}</h4>
                             {{--<div class="form-group col-md-6 col-xs-6">--}}
