d6ae3bbf799505536bb9865d8e09cf8b7a72f4a1
msbir <maayan@b2cprint.com>
2017-12-22 18:21:03 +0200

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pulling latest changes
+git pull origin master
+# Install composer while optimizing
+composer install --optimize-autoloader
+# Optimizing cache
+php artisan cache:clear
+php artisan route:cache
+php artisan config:cache
+php artisan view:cache
+# Migrate db changes
+php artisan migrate --force
