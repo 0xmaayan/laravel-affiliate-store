80a81f5f8b213b02fec227f3a8ec22fc0b6fb4fb
msbir <maayan@b2cprint.com>
2017-12-23 13:36:16 +0200

    brand route typo

--- resources/views/pages/brands/index.blade.php
+++ resources/views/pages/brands/index.blade.php
@@ -8,7 +8,7 @@
                 @foreach($brands as $brand)
                     <div class="col-md-3" style="">
                         <div class="card text-center">
-                            <a href="{{route('admin.categories.edit',['id' => $brand->id ])}}">
+                            <a href="brands/{{$brand->slug}}">
                                 <img class="card-img-top" style="margin:0 auto;width: 200px;height:200px;object-fit: contain;" src="{{asset('uploads/brands/'.$brand->image)}}" alt="category image">
                                 <div class="card-block">
                                     <h4 class="card-title">{{$brand->name}}</h4>
