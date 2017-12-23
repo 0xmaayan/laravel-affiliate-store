1246dbf0971cb3496f6e9ac44a745611b08806a1
msbir <maayan@b2cprint.com>
2017-12-23 13:29:05 +0200

    deactivate hp slider image upload

--- resources/views/admin/content/edit.blade.php
+++ resources/views/admin/content/edit.blade.php
@@ -20,37 +20,31 @@
                 </div>
 
                 {!! Form::open(['route' => ['admin.content.update',$content->id ],'files' => true ,'method' => 'PUT']) !!}
-                <div class="form-group col-md-4">
+               {{-- <div class="form-group col-md-4">
                     <label>{!! Form::label('image 1') !!}</label>
                     {!! Form::file('image[]') !!}
-                    {{--{!! Form::text('image_text[]',$content['content'][0],['class' => 'form-control']) !!}--}}
+                    {!! Form::text('image_text[]',$content['content'][0],['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group col-md-4">
                     <label>{!! Form::label('image 2') !!}</label>
                     {!! Form::file('image[]') !!}
-                    {{--{!! Form::text('image_text[]',$content['content'][1],['class' => 'form-control']) !!}--}}
+                    {!! Form::text('image_text[]',$content['content'][1],['class' => 'form-control']) !!}
                 </div>
                 <div class="form-group col-md-4">
                     <label>{!! Form::label('image 3') !!}</label>
                     {!! Form::file('image[]') !!}
-                    {{--{!! Form::text('image_text[]',$content['content'][2],['class' => 'form-control']) !!}--}}
-                </div>
-                @if($content[0]['files'][0])
+                    {!! Form::text('image_text[]',$content['content'][2],['class' => 'form-control']) !!}
+                </div>--}}
                 <div class="form-group col-md-4">
-                    <img style="width:100%" src="{{asset('uploads/home_slider/'.$content['files'][0])}}" alt="image 1">
+                    <img style="width:100%" src="{{asset('images/home_slider/slider0.png')}}" alt="image 1">
                 </div>
-                @endif
-                @if($content[0]['files'][1])
                 <div class="form-group col-md-4">
-                    <img style="width:100%" src="{{asset('uploads/home_slider/'.$content['files'][1])}}" alt="image 2">
+                    <img style="width:100%" src="{{asset('images/home_slider/slider11.png')}}" alt="image 2">
                 </div>
-                @endif
-                @if($content[0]['files'][2])
                 <div class="form-group col-md-4">
-                    <img style="width:100%" src="{{asset('uploads/home_slider/'.$content['files'][2])}}" alt="image 3">
+                    <img style="width:100%" src="{{asset('images/home_slider/slider2.png')}}" alt="image 3">
                 </div>
-                @endif
-                {!! Form::submit('Update',['class' => 'btn btn-default col-md-12']) !!}
+                {{--{!! Form::submit('Update',['class' => 'btn btn-default col-md-12']) !!}--}}
                 {!! Form::close() !!}
         @elseif ($content->id == 2)
             <div class="col-md-12">
