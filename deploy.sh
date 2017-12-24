8313b9e52d8c819ebab7fc6b2380e104714fbffd
maayans <maayansavir@gmail.com>
2017-12-24 21:28:17 +0200

    modify deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#shutdown the app
+php artisan down
 # Pulling latest changes
 git pull origin master
 # Install composer while optimizing
@@ -11,3 +13,5 @@ php artisan config:cache
 php artisan view:cache
 # Migrate db changes
 php artisan migrate --force
+# Turn on the app
+php artisan up
