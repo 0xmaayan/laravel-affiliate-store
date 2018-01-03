287c25f23b5d9a764dec867d9d0661517927efd8
maayans <maayansavir@gmail.com>
2018-01-03 20:33:45 +0200

    mail view

--- resources/views/emails/subscribe/new.blade.php
+++ resources/views/emails/subscribe/new.blade.php
@@ -1,12 +1,15 @@
 @component('mail::message')
-# Introduction
 
-The body of your message.
+# Hello,
 
-@component('mail::button', ['url' => ''])
-Button Text
+Thank you for register to {{config('app.name')}} newsletter.
+
+By being part of our space, you will be informed when a new product is arrived,
+when a nice post will go live and on other promotions we will have on our site.
+
+@component('mail::button', ['url' => url('/'), 'color' => 'green'])
+Stay tuned
 @endcomponent
-
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
