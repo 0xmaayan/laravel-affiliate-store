dd30cf3472866b86b635fb094030bb2e8f20c79e
maayans <maayansavir@gmail.com>
2018-01-03 20:34:27 +0200

    make mailable new subscribe mail

--- app/Mail/NewSubscribe.php
+++ app/Mail/NewSubscribe.php
@@ -5,10 +5,11 @@ namespace App\Mail;
 
 use App\Subscribe;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class NewSubscribe extends Mailable {
+class NewSubscribe extends Mailable implements ShouldQueue{
   use Queueable, SerializesModels;
 
   protected $subscribe;
@@ -23,14 +24,16 @@ class NewSubscribe extends Mailable {
   {
     $this->subscribe = $subscribe;
   }
-
+/*
+ *
+ */
   public function build(){
+
     return $this->subject('Hi There!!')
-        ->from(config('mail.from.address', config("app.name"))
-        ->markdown('emails.subscribes.new')
-        ->attach('/path/to/file', [
-          'as' => 'name.pdf',
-          'mime' => 'application/pdf',
-        ]));
+        ->from(config('mail.from.name'), config('app.name'))
+        ->markdown('emails.subscribe.new')
+        ->with('subscribe', $this->subscribe)
+        ->attach('/public/images/footer-logo.png');
+
   }
 }
