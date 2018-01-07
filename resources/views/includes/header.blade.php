c143b21145cf95b23e2fe55bc68d1f24ae5052f5
maayans <maayansavir@gmail.com>
2018-01-07 21:08:57 +0200

    img src to srcset bug

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,7 +3,7 @@
     <div class="col-md-12">
         <div id="logo" class="col-md-12">
             <h1><a href="{{url('/')}}">
-                    <img src="{{asset('uploads/logo/'.$settings['logo'])}}" alt="lostInSpace_logo" />
+                    <img srcset="{{asset('uploads/logo/'.$settings['logo'])}}" alt="lostInSpace_logo" />
                 </a>
             </h1>
         </div>
