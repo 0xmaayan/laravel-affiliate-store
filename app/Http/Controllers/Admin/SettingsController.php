6d0e23d373cce9a4df880b92b30cda627325e9ff
maayans <maayansavir@gmail.com>
2018-02-08 16:49:15 +0200

    refactore file

--- app/Http/Controllers/Admin/SettingsController.php
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -8,19 +8,18 @@ use App\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
-class SettingsController extends AdminController
-{
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-      $settings = Setting::firstOrCreate(['id' => 1]);
+class SettingsController extends AdminController {
+  /**
+   * Show the application dashboard.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $settings = Setting::firstOrCreate(['id' => 1]);
 
-      return view('admin.settings.index',compact('settings'));
-    }
+    return view('admin.settings.index', compact('settings'));
+  }
 
 
   /**
@@ -29,25 +28,26 @@ class SettingsController extends AdminController
    * @param SettingsRequest|Request $request
    * @return \Illuminate\Http\Response
    */
-    public function update(SettingsRequest $request)
+  public function update(SettingsRequest $request)
+  {
+
+    $data = [
+      'title'           => $request->title,
+      'seo_description' => $request->seo_description,
+      'seo_keywords'    => $request->seo_keywords,
+      'email'           => $request->email,
+      'instagram'       => $request->instagram,
+      'pinterest'       => $request->pinterest,
+    ];
+
+    if ($request->file('logo'))
     {
-
-      $data = [
-        'title' => $request->title,
-        'seo_description' => $request->seo_description,
-        'seo_keywords' => $request->seo_keywords,
-        'email' => $request->email,
-        'instagram' => $request->instagram,
-        'pinterest' => $request->pinterest,
-      ];
-
-      if($request->file('logo')){
-        $data['logo'] = $request->file('logo')->getClientOriginalName();
-        $request->logo->move(public_path('/uploads/logo'), $data['logo']);
-      }
-
-      Setting::updateOrCreate(['id' => 1],$data);
-
-      return Redirect::route('admin.settings');
+      $data['logo'] = $request->file('logo')->getClientOriginalName();
+      $request->logo->move(public_path('/uploads/logo'), $data['logo']);
     }
+
+    Setting::updateOrCreate(['id' => 1], $data);
+
+    return Redirect::route('admin.settings');
+  }
 }
