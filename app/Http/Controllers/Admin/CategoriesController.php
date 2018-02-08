f26aed81c391062a7fa21f7c1d20c98661d5507c
maayans <maayansavir@gmail.com>
2018-02-08 16:44:09 +0200

    organize file

--- app/Http/Controllers/Admin/CategoriesController.php
+++ app/Http/Controllers/Admin/CategoriesController.php
@@ -9,30 +9,29 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Yajra\DataTables\DataTables;
 
-class CategoriesController extends AdminController
-{
+class CategoriesController extends AdminController {
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-      $categories = Category::all();
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    $categories = Category::all();
 
-      return view('admin.categories.index',compact('categories'));
-    }
+    return view('admin.categories.index', compact('categories'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('admin.categories.create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('admin.categories.create');
+  }
 
   /**
    * Store a newly created resource in storage.
@@ -40,35 +39,36 @@ class CategoriesController extends AdminController
    * @param \App\Http\Controllers\Admin\CategoryRequest|CategoryRequest $request
    * @return \Illuminate\Http\Response
    */
-    public function store(CategoryRequest $request)
+  public function store(CategoryRequest $request)
+  {
+    $data = [
+      'name' => $request->name,
+    ];
+
+    $category = Category::create($data);
+
+    $public_dir = $this->createFolder('uploads/categories/'.$category->id.'/');
+
+    if ($request->file('image'))
     {
-      $data = [
-        'name' => $request->name,
-      ];
-
-      $category = Category::create($data);
-
-      $public_dir = $this->createFolder('uploads/categories/'.$category->id.'/');
-
-      if($request->file('image')){
-        $data = $this->createImage($request->file('image'),$public_dir);
-      }
-
-      $category->update($data);
-
-      return Redirect::route('admin.categories.index');
+      $data = $this->createImage($request->file('image'), $public_dir);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    $category->update($data);
+
+    return Redirect::route('admin.categories.index');
+  }
+
+  /**
+   * Display the specified resource.
+   *
+   * @param  int $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
   /**
    * Show the form for editing the specified resource.
@@ -77,24 +77,28 @@ class CategoriesController extends AdminController
    * @param Request $request
    * @return \Illuminate\Http\Response
    */
-    public function edit($id, Request $request)
-    {
-        $category = Category::findOrFail($id);
-        $products = $category->products()->get();
+  public function edit($id, Request $request)
+  {
+    $category = Category::findOrFail($id);
+    $products = $category->products()->get();
 
-      if ($request->ajax()){
-        return Datatables::of($products)
-          ->editColumn('link', function ($product){
+    if ($request->ajax())
+    {
+      return Datatables::of($products)->editColumn(
+          'link',
+          function ($product) {
             return '<a href="'.$product->link.'">Product Link</a>';
-          })
-          ->addColumn('action', function ($product){
-            return '<a class="btn btn-sm btn-warning" href="'.route('admin.products.edit',$product->id).'">Edit</a>';
-          })
-          ->rawColumns(['link','action'])
-          ->make(true);
-      }
-        return view('admin.categories.edit', compact('category','products'));
+          }
+        )->addColumn(
+          'action',
+          function ($product) {
+            return '<a class="btn btn-sm btn-warning" href="'.route('admin.products.edit', $product->id).'">Edit</a>';
+          }
+        )->rawColumns(['link', 'action'])->make(true);
     }
+
+    return view('admin.categories.edit', compact('category', 'products'));
+  }
 
   /**
    * Update the specified resource in storage.
@@ -103,37 +107,38 @@ class CategoriesController extends AdminController
    * @param  int $id
    * @return \Illuminate\Http\Response
    */
-    public function update(CategoryRequest $request, $id)
+  public function update(CategoryRequest $request, $id)
+  {
+
+    $data = [
+      'name' => $request->name,
+    ];
+    $category = Category::findOrFail($id);
+
+    $public_dir = $this->createFolder('uploads/categories/'.$id.'/');
+
+    if ($request->file('image'))
     {
-
-      $data = [
-        'name' => $request->name,
-      ];
-      $category = Category::findOrFail($id);
-
-      $public_dir = $this->createFolder('uploads/categories/'.$id.'/');
-
-      if($request->file('image')){
-        $data = $this->createImage($request->file('image'),$public_dir);
-      }
-
-      $category->update($data);
-
-      return redirect()->back();
+      $data = $this->createImage($request->file('image'), $public_dir);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $category = Category::findOrFail($id);
+    $category->update($data);
 
-        $category->delete();
+    return redirect()->back();
+  }
 
-        return Redirect::route('admin.categories.index');
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $category = Category::findOrFail($id);
+
+    $category->delete();
+
+    return Redirect::route('admin.categories.index');
+  }
 }
