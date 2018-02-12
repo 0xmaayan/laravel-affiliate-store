fa8468684fe675e774ba215f4101d7695b34864c
maayans <maayansavir@gmail.com>
2018-02-12 21:03:17 +0200

    call style.css last

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -21,9 +21,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap-theme.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link rel="stylesheet" href="{{asset('css/costume.css')}}">
     <link rel="stylesheet" href="{{asset('css/colors/blue.css')}}" id="colors">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 
     @stack('styles')
     <!--[if lt IE 9]>
