75836b7cbabb2bc33b9684eb7ca5f7ed35502a12
maayans <maayansavir@gmail.com>
2018-02-20 20:46:00 +0200

    init

--- resources/views/partials/products_module.blade.php
+++ resources/views/partials/products_module.blade.php
@@ -1,6 +1,6 @@
-<div class="card col-xs-6 col-md-3 col-sm-4 text-center product" style="margin-bottom: 20px;" onclick="trackingClick({{$product->id}})">
+<div class="card col-xs-12 col-md-3 col-sm-4 text-center product" style="margin-bottom: 20px;" onclick="trackingClick({{$product->id}})">
     <a href="{{$product->link}}" target="_blank">
-        <img src="{{$product->main_image}}" alt="{{$product->name}}" style="max-width: 220px;max-height: 220px;margin: 0 auto;"/>
+        <img src="{{$product->main_image}}" alt="{{$product->name}}" style="width: 220px;height: 220px;margin: 0 auto;"/>
     </a>
     <section style="padding-bottom: 0">
         {{--<h4 class="card-title">{{$product->name}}</h4>--}}
