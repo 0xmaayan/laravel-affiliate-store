fc61c7a59d891cf7b44714160745f20527b73ccc
maayans <maayansavir@gmail.com>
2018-02-20 19:24:45 +0200

    product model view

--- resources/views/pages/products/index.blade.php
+++ resources/views/pages/products/index.blade.php
@@ -5,18 +5,7 @@
         <div class="row">
             <div class="col-md-12">
                 @foreach($products as $product)
-                    <div class="card col-xs-6 col-md-3 col-sm-4 text-center product" style="margin-bottom: 20px;" onclick="trackingClick({{$product->id}})">
-                        <a href="{{$product->link}}" target="_blank">
-                            <img src="{{$product->main_image}}" alt="{{$product->name}}" style="width: 220px;height: 220px;"/>
-                        </a>
-                        <section style="padding-bottom: 0">
-                            {{--<h4 class="card-title">{{$product->name}}</h4>--}}
-                            <p class="card-text" style="margin:0;font-size:20px !important;">${{$product->price}}</p>
-                            {{--<a href="{{$product->link}}" class="btn-block shopNow" target="_blank">
-                                <i class="fa fa-shopping-cart"></i> Shop Now
-                            </a>--}}
-                        </section>
-                    </div>
+                    @include('partials.products_module')
                 @endforeach
             </div>
         </div>
