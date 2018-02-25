51477d1dd5a1e53b49af5e088c1c31c51e2cb8d3
maayans <maayansavir@gmail.com>
2018-02-25 16:17:21 +0200

    new images

--- resources/views/pages/homepage/parallax.blade.php
+++ resources/views/pages/homepage/parallax.blade.php
@@ -1,7 +1,7 @@
 
 <div class="parallax-banner fullwidth-element"  data-background="#000" data-opacity="0.45" data-height="200">
-    <img src="" alt="" />
-    <div class="parallax-overlay"></div>
-    <div class="parallax-title">End of season sale <span>Up to 35% off Women’s Denim</span></div>
+    <img src="{{asset('images/followusparallax.jpg')}}" alt="" />
+    {{--<div class="parallax-overlay"></div>
+    <div class="parallax-title">End of season sale <span>Up to 35% off Women’s Denim</span></div>--}}
 </div>
 
--- resources/views/pages/homepage/slider.blade.php
+++ resources/views/pages/homepage/slider.blade.php
@@ -7,7 +7,7 @@
             <ul>
                 <!-- Slide 1  -->
                 <li data-transition="fade" data-slotamount="7" data-masterspeed="1000">
-                    <img src="{{asset('images/home_slider/slider0.png')}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+                    <img src="{{asset('images/home_slider/1.jpg')}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
                     {{--<div class="caption dark sfb fadeout" data-x="850" data-y="200" data-speed="400" data-start="800"  data-easing="Power4.easeOut">--}}
                         {{--<h2>Urban Style</h2>--}}
                         {{--<h3>Every cut and colour</h3>--}}
@@ -16,7 +16,7 @@
                 </li>
                 <!-- Slide 2  -->
                 <li data-transition="zoomout" data-slotamount="7" data-masterspeed="1500" >
-                    <img src="{{asset('images/home_slider/slider11.png')}}"  alt="slidebg1"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+                    <img src="{{asset('images/home_slider/2.jpg')}}"  alt="slidebg1"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
                     {{--<div class="caption sfb fadeout" data-x="500" data-y="400" data-speed="400" data-start="800"  data-easing="Power4.easeOut">--}}
                         {{--<h2>Dress Sharp</h2>--}}
                         {{--<h3>Learn from the classics</h3>--}}
@@ -29,7 +29,7 @@
                     <div class="caption dark sfb fadeout" data-x="970" data-y="250" data-speed="400" data-start="800"  data-easing="Power4.easeOut">
                         {{--<h2>New In</h2>--}}
                         {{--<h3>Pants and T-Shirts</h3>--}}
-                        <a href="mailto:{{$settings['email']}}" class="caption-btn">JOIN NOW!</a>
+                        {{--<a href="mailto:{{$settings['email']}}" class="caption-btn">JOIN NOW!</a>--}}
                     </div>
                 </li>
             </ul>
