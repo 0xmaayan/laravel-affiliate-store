dfaa9816b8adecfbcc8bd6f0c7ec4ed543e951ba
maayans <maayansavir@gmail.com>
2018-04-21 18:58:22 +0300

    ignore specific segment on sitemap xml

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
 
 class GenerateSitemap extends Command
 {
@@ -30,6 +31,13 @@ class GenerateSitemap extends Command
   {
     // modify this to your own needs
     SitemapGenerator::create(config('app.url'))
+      ->hasCrawled(function (Url $url) {
+        if ($url->segment(1) === 'cdn-cgi') {
+          return;
+        }
+
+        return $url;
+      })
       ->writeToFile(public_path('sitemap.xml'));
   }
 }
