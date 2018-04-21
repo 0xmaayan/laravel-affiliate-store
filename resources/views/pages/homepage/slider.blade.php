60da246e3c211f9557526488c88f66caa926c592
maayans <maayansavir@gmail.com>
2018-04-21 13:16:50 +0300

    refactor slider images

--- resources/views/pages/homepage/slider.blade.php
+++ resources/views/pages/homepage/slider.blade.php
@@ -7,7 +7,7 @@
             <ul>
                 <!-- Slide 1  -->
                 <li data-transition="fade" data-slotamount="7" data-masterspeed="1000">
-                    <img src="{{asset('images/home_slider/1.jpg')}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+                    <img src="{{asset('images/home_slider/2.jpg')}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
                     {{--<div class="caption dark sfb fadeout" data-x="850" data-y="200" data-speed="400" data-start="800"  data-easing="Power4.easeOut">--}}
                         {{--<h2>Urban Style</h2>--}}
                         {{--<h3>Every cut and colour</h3>--}}
@@ -16,7 +16,7 @@
                 </li>
                 <!-- Slide 2  -->
                 <li data-transition="zoomout" data-slotamount="7" data-masterspeed="1500" >
-                    <img src="{{asset('images/home_slider/2.jpg')}}"  alt="slidebg1"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+                    <img src="{{asset('images/home_slider/cover_1.jpg')}}"  alt="slidebg1"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
                     {{--<div class="caption sfb fadeout" data-x="500" data-y="400" data-speed="400" data-start="800"  data-easing="Power4.easeOut">--}}
                         {{--<h2>Dress Sharp</h2>--}}
                         {{--<h3>Learn from the classics</h3>--}}
@@ -25,7 +25,7 @@
                 </li>
                 <!-- Slide 3  -->
                 <li data-transition="fadetotopfadefrombottom" data-slotamount="7" data-masterspeed="1000">
-                    <img src="{{asset('images/home_slider/slider2.png')}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
+                    <img src="{{asset('images/home_slider/art.jpeg')}}"  alt="darkblurbg"  data-bgfit="cover" data-bgposition="left top" data-bgrepeat="no-repeat">
                     <div class="caption dark sfb fadeout" data-x="970" data-y="250" data-speed="400" data-start="800"  data-easing="Power4.easeOut">
                         {{--<h2>New In</h2>--}}
                         {{--<h3>Pants and T-Shirts</h3>--}}
